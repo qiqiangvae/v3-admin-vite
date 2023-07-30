5b01b4a21f438caddd62b4a045ede2db500a5538
Quince <qiqiangvae@foxmail.com>
2023-07-30 23:08:02 +0800

    fix

--- src/views/dashboard/components/Admin.vue
+++ src/views/dashboard/components/Admin.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-row v-for="(group, g_idx) in optionGroup" :key="g_idx">
-      <el-col :span="8" v-for="(item, idx) in group" :key="idx">
+      <el-col :span="24 / colCount" v-for="(item, idx) in group" :key="idx">
         <el-card class="box-card">
           <v-chart :option="item" class="chart-grid" />
         </el-card>
@@ -16,14 +16,15 @@ import { ref } from "vue"
 
 const optionGroup = ref<EchartsOptions[][]>([])
 
+const colCount = 3
+
 const createCode = () => {
   getEchartsDashboardApi().then((res) => {
     const len = res.data.length //len为数组的长度
-    const n = 3 // 假设每行显示4个
-    const Num = len % n === 0 ? len / n : Math.floor(len / n + 1) //得出多少份
+    const Num = len % colCount === 0 ? len / colCount : Math.floor(len / colCount + 1) //得出多少份
     for (let i = 0; i < Num; i++) {
       // slice() 方法返回一个从开始到结束（不包括结束）的数组。且原始数组不会被修改。
-      const newArr = res.data.slice(i * n, i * n + n) //得到每份的数据
+      const newArr = res.data.slice(i * colCount, i * colCount + colCount) //得到每份的数据
       optionGroup.value.push(newArr) //往res数组里加数据
     }
   })
